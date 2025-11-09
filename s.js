f1e82480b5284c6d74d4df74c5c99d0d2c77ee9b
TomserAliKhan <tomserali6500@gmail.com>
2025-11-09 19:26:27 +0530

    s

--- s.js
+++ s.js
@@ -3,86 +3,83 @@ let url = "https://quranapi.pages.dev/api/2/7.json";
 
 let to = document.getElementById("to");
 let from = document.getElementById("from");
-let output = document.querySelector('.output')
+let out = document.querySelector('.out')
 let btn = document.querySelector(".submit input");
 let inp = document.getElementById("inp")
 let tost = document.getElementById("tost")
 let done = document.getElementById("done")
-let urls = []
-let surah = ''
 
+let surah = ''
 btn.addEventListener("click", function (e) {
     e.preventDefault();
-    tost.classList.remove('hide')
-    output.innerHTML = ''
+      out.innerHTML = "";
 
-    if (isNaN(to.value) || to.value === "") {
+    tost.classList.remove('hide')
+
+    if (isNaN(to.value) || to.value === "" || isNaN(from.value) || from.value === "") {
         alert('use a number')
         tost.classList.add('hide')
         return;
     }
-   
-    if (!isNaN(inp.value)) {
-        surah= Number(inp.value);
-        if (surah < 1 || surah > 114) {
-            alert('Surah number must be between 1 and 114');
-            tost.classList.add('hide')
-            return;
-        }
-        if (from.value === "" || isNaN(from.value) || from.value < 1) {
-            from.value = 1;
-        }
-        
-
+    if (from.value > to.value) {
+        alert('From must be less than To')
+        tost.classList.add('hide')
+        return;
     }
 
+    if (!isNaN(inp.value)) {
+        surah = inp.value
+    }
+    let urls = []// Reset urls array on each click 
     for (let i = from.value; i <= to.value; i++) {
         urls.push(`https://quranapi.pages.dev/api/${surah}/${i}.json`)
 
 
 
     }
-
-    let datas = urls.map(url =>
-
-
-        fetch(url).then(res => res.json())
-
-    )
-
-    Promise.all(datas)
-        .then(data => {
-
-            data.map((d) => {
-                console.log(d);
-
-
-                let div = document.createElement('div');
-                div.classList.add('out-2')
-                let p = document.createElement('p');
-                p.innerText = d.arabic1;
-                div.appendChild(p)
-                let audio = document.createElement('audio');
-                audio.setAttribute('src', d.audio[4].originalUrl);
-                audio.setAttribute('controls', true);
-                div.appendChild(audio)
-                output.appendChild(div)
-                tost.classList.add('hide')
-                done.classList.remove('hide')
-                setTimeout(() =>
-                    done.classList.add('hide'), 3000)
+    try {
+        let datas = Promise.all(urls.map(url =>
+            fetch(url).then(res => res.json())))
 
 
 
-            })
-        })
-        .catch(error => console.error('API request failed:', error));
+
+        .then(data=>data.map(d => {
+            console.log(d);
+
+
+            let div = document.createElement('div');
+            div.classList.add('out-2')
+            let audio = document.createElement('audio');
+            audio.setAttribute('src', d.audio[4].originalUrl);
+            audio.setAttribute('controls', true);
+            let p = document.createElement('p');
+            p.textContent = d.arabic1;
+            div.appendChild(p)
+            div.appendChild(audio)
+            out.appendChild(div)
 
 
 
 
 
+        }))
+      tost.classList.add('hide')
+      done.classList.remove('hide')     
+      setTimeout(() => {
+        done.classList.add('hide')
+      }, 3000);
+}
+ catch(error) {
+        console.log( error)
+    }
 
+
+
+
+
+ 
+   
 
 
 
